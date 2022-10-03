e7e9d8def51a619e0b31b44a849e8f22e26789fc
jatin.pal <jatin.pal@bitcs.in>
2022-10-04 02:58:30 +0530

    bug

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -91,86 +91,86 @@ export class SearchService implements OnModuleInit {
         }
     }
 
-    init(uri: any, id: string) {
-        var self = this;
-        const magnetURI = "magnet:?xt=urn:btih:B9F0A8BAF8B647D4754BE630A91401F2EB678E69&dn=House.of.the.Dragon.S01E06.WEB.x264-PHOENiX&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.birkenwald.de%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentor.org%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2970%2Fannounce&tr=https%3A%2F%2Ftracker.foreverpirates.co%3A443%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"
-        const client = new WebTorrent();
-        client.add(magnetURI, function (torrent) {
+    // init(uri: any, id: string) {
+    //     var self = this;
+    //     const magnetURI = "magnet:?xt=urn:btih:B9F0A8BAF8B647D4754BE630A91401F2EB678E69&dn=House.of.the.Dragon.S01E06.WEB.x264-PHOENiX&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.birkenwald.de%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentor.org%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2970%2Fannounce&tr=https%3A%2F%2Ftracker.foreverpirates.co%3A443%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce"
+    //     const client = new WebTorrent();
+    //     client.add(magnetURI, function (torrent) {
 
-            // console.log(torrent);
+    //         // console.log(torrent);
 
-            // el.innerHTML = torrent;
-            // console.log(torrent.torrentFileBlobURL); 
+    //         // el.innerHTML = torrent;
+    //         // console.log(torrent.torrentFileBlobURL); 
 
-            torrent.on("download", (s) => {
-                let res = downloadSpeed(torrent.downloadSpeed);
-                let speed = `${res}/s`;
-                let name = torrent.name
-                let progress = torrent.progress
+    //         torrent.on("download", (s) => {
+    //             let res = downloadSpeed(torrent.downloadSpeed);
+    //             let speed = `${res}/s`;
+    //             let name = torrent.name
+    //             let progress = torrent.progress
 
-                // console.log({
-                //     speed, name, progress
-                // });
-            })
-            torrent.on('done', function () {
+    //             // console.log({
+    //             //     speed, name, progress
+    //             // });
+    //         })
+    //         torrent.on('done', function () {
 
-                console.log('torrent download finished')
-                console.log(torrent.files);
+    //             console.log('torrent download finished')
+    //             console.log(torrent.files);
 
-                var file = torrent.files.find(file => file.name.endsWith('.mkv'))
-                // console.log(file);
-                self.Buffer = file;
-                // file.getBlob(()=>{})
-                // file.getBuffer((err, buff) => {
+    //             var file = torrent.files.find(file => file.name.endsWith('.mkv'))
+    //             // console.log(file);
+    //             self.Buffer = file;
+    //             // file.getBlob(()=>{})
+    //             // file.getBuffer((err, buff) => {
 
-                //     if (!err) {
-                //         console.log(buff);
-                //         //  self.repo.add(id, buff) 
-                //         self.Buffer = buff;
-                //         return {
-                //             buffer: buff
-                //         }
-                //     }
-                //     else console.log(err); 
-                // })
-
-
-                // torrent.files[0].getBlobURL(function (err, url) {
-                //      ;
-                //     console.log(url);
-                //     console.log(err);
-                // })
-            })
+    //             //     if (!err) {
+    //             //         console.log(buff);
+    //             //         //  self.repo.add(id, buff) 
+    //             //         self.Buffer = buff;
+    //             //         return {
+    //             //             buffer: buff
+    //             //         }
+    //             //     }
+    //             //     else console.log(err); 
+    //             // })
 
 
-        })
+    //             // torrent.files[0].getBlobURL(function (err, url) {
+    //             //      ;
+    //             //     console.log(url);
+    //             //     console.log(err);
+    //             // })
+    //         })
 
-        client.on("error", () => {
-            console.log("error");
-        })
 
-        client.on("torrent", (torrent) => {
+    //     })
 
-            console.log("ready");
+    //     client.on("error", () => {
+    //         console.log("error");
+    //     })
 
-            // saveByteArray("Sample Report.mp4", buffer);
-            // Display the file by adding it to the DOM.
-            // Supports video, audio, image files, and more!
-        })
-    }
+    //     client.on("torrent", (torrent) => {
+
+    //         console.log("ready");
+
+    //         // saveByteArray("Sample Report.mp4", buffer);
+    //         // Display the file by adding it to the DOM.
+    //         // Supports video, audio, image files, and more!
+    //     })
+    // }
 
 
 
 
 }
 
-const downloadSpeed = (num: any) => {
-    const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-    const neg = num < 0
-    if (neg) num = -num
-    if (num < 1) return (neg ? '-' : '') + num + ' B'
-    const exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1)
-    const unit = units[exponent]
-    num = Number((num / Math.pow(1000, exponent)).toFixed(2))
-    return (neg ? '-' : '') + num + ' ' + unit
-}
+// const downloadSpeed = (num: any) => {
+//     const units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+//     const neg = num < 0
+//     if (neg) num = -num
+//     if (num < 1) return (neg ? '-' : '') + num + ' B'
+//     const exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), units.length - 1)
+//     const unit = units[exponent]
+//     num = Number((num / Math.pow(1000, exponent)).toFixed(2))
+//     return (neg ? '-' : '') + num + ' ' + unit
+// }
